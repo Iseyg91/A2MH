d712341b27e8af412c425306d374ba35667c9b2a
Iseyg91 <gokalpahmet74@gmail.com>
2025-05-06 20:56:01 +0200

    Add files via upload

---
+++ build/build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Créer le répertoire build s'il n'existe pas
+mkdir -p build
+
+# Copier les fichiers HTML du dossier pages vers build
+cp -R pages/* build/
+
+# Copier les images
+cp -R images build/
+
+# Copier les Images n°2
+cp -R images2 build/
+
+# Copier les styles
+cp -R styles build/
